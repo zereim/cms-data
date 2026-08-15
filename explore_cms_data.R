8fba4a0651f10c6844942f73fb7b39912acc2201
zereim <wtruskolaski@gmail.com>
2026-08-14 21:53:24 -0400

    Cleaning the column names and working on cleaning values in columns

--- explore_cms_data.R
+++ explore_cms_data.R
@@ -3,17 +3,35 @@ library(janitor)
 library(readr)
 library(logger)
 
+
 df <- as_tibble(read_csv("/Users/william/Library/CloudStorage/Dropbox/Rscripts/cms-data/PATIENT_REPORTED_OUTCOMES_FACILITY(1).csv"))
+
+# df <- clean_names(df, "lower_camel")
+
+# Cleaning 
+df <- df |> 
+  clean_names("snake") |> 
+  mutate(voluntary_reporting = recode
+        (voluntary_reporting, "Yes" = TRUE, "No" = FALSE))
+
+# turn all of the Not Available into NA 
 
 
 # cleaning names of the columns in the dataframe 
-df |> 
-  clean_names()
+# df |> 
+#   clean_names("lower_camel") |> 
+#   select(Facility Name, Address, City/Town,
+#          State, County/Parish, Measure ID, 
+#          Measure Name) |> 
+#   mutate(tolower())
+
+  
 
 View(df)
 
-hospital_cols <- select(df, contains("hospital"))
+hospital_cols <- select(df, contains("HOSPITAL"))
 
+View(hospital_cols)
 
 #df %>% 
 #  select(contains("hospital")) %>% 
