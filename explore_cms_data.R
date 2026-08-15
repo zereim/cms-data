b73b0481ee7673807c42793164ca2af48a045f4c
zereim <wtruskolaski@gmail.com>
2026-08-15 13:48:22 -0400

    updating packages to make current code work with new dplyr methods

--- explore_cms_data.R
+++ explore_cms_data.R
@@ -6,15 +6,22 @@ library(logger)
 
 df <- as_tibble(read_csv("/Users/william/Library/CloudStorage/Dropbox/Rscripts/cms-data/PATIENT_REPORTED_OUTCOMES_FACILITY(1).csv"))
 
-# df <- clean_names(df, "lower_camel")
+# d?f <- clean_names(df, "lower_camel")
 
 # Cleaning 
 df <- df |> 
   clean_names("snake") |> 
   mutate(voluntary_reporting = recode
-        (voluntary_reporting, "Yes" = TRUE, "No" = FALSE))
+        (voluntary_reporting, "Yes" = TRUE,
+         "No" = FALSE)) |> 
+  mutate(
+    score = score |> 
+      replace_when(score == "Not Available" ~ NA)
+  )
+  
 
 # turn all of the Not Available into NA 
+
 
 
 # cleaning names of the columns in the dataframe 
