862f48d2fe177847ccfa1ad07c84d9066a7a6a9d
zereim <wtruskolaski@gmail.com>
2026-08-14 20:40:38 -0400

    changing a few things

--- explore_cms_data.R
+++ explore_cms_data.R
@@ -2,12 +2,19 @@ library(tidyverse)
 library(janitor)
 library(readr)
 library(logger)
+
 df <- as_tibble(read_csv("/Users/william/Library/CloudStorage/Dropbox/Rscripts/cms-data/PATIENT_REPORTED_OUTCOMES_FACILITY(1).csv"))
 
 
 # cleaning names of the columns in the dataframe 
-df %>% 
+df |> 
   clean_names()
 
 View(df)
 
+hospital_cols <- select(df, contains("hospital"))
+
+
+#df %>% 
+#  select(contains("hospital")) %>% 
+#  View()
